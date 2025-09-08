e3403dc4bda5089da96b6c501ccdbfc79721d167
Nadhon(LADINO) <70290966+nadhon@users.noreply.github.com>
2025-09-08 19:01:10 -0300

    arquivos do vue

---
+++ App.vue
@@ -0,0 +1,12 @@
+const { createApp } = Vue
+
+createApp({
+    goto(Home) {
+        if (Home === 'login') {
+            alert("Indo para Login")
+        }
+        else if (Home === 'register') {
+            alert("Indo para Register")
+        }
+    }
+})
